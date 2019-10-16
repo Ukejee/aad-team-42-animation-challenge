1ec9e679396ca494bf1a2c6444286ac5521c1941
ukejee <ukejee3@gmail.com>
2019-10-16 16:29:58 +0100

    Initial commit

---
+++ app/src/main/java/com/aad_team_42/travelmanticsrebranded/MainActivity.java
@@ -0,0 +1,14 @@
+package com.aad_team_42.travelmanticsrebranded;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
