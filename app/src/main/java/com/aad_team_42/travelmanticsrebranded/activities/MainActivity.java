4dd5520a6bb544072bd3ae393f1192980b8eb957
Chukwu Gideon Emeka <chuksgid@gmail.com>
2019-10-19 15:22:44 +0100

    Revert "Signin page"

--- app/src/main/java/com/aad_team_42/travelmanticsrebranded/activities/MainActivity.java
+++ app/src/main/java/com/aad_team_42/travelmanticsrebranded/activities/MainActivity.java
@@ -1,51 +1,16 @@
 package com.aad_team_42.travelmanticsrebranded.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
-import android.view.Menu;
 
 import com.aad_team_42.travelmanticsrebranded.R;
-import com.aad_team_42.travelmanticsrebranded.activities.adapters.ViewPagerAdapter;
-import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
-    Toolbar toolbar;
-    TabLayout tabLayout;
-    private int tabIcons[] = {
-            R.drawable.explore, R.drawable.favorite
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        ViewPager pager = findViewById(R.id.view_pager);
-        tabLayout = findViewById(R.id.tablayout);
-        ViewPagerAdapter pagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
-        pager.setAdapter(pagerAdapter);
-        tabLayout.setupWithViewPager(pager);
-
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        setUpTabIcons();
-
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-
-    private void setUpTabIcons() {
-        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
-        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
     }
 }
