630e2a11f68b990c96d7d575708ef9f4699fa5a6
Kulloveth <kulloveth@gmail.com>
2019-10-21 09:19:15 +0200

    some fixes

--- app/src/main/java/com/aad_team_42/travelmanticsrebranded/views/activities/SplashScreenActivity.java
+++ app/src/main/java/com/aad_team_42/travelmanticsrebranded/views/activities/SplashScreenActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
 
 import com.aad_team_42.travelmanticsrebranded.R;
 
@@ -14,7 +15,6 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
