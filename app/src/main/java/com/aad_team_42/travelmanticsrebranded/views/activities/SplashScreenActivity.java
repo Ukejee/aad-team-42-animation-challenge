bbae5132f10e26c591916282c1eb3037571a4c82
ukejee <ukejee3@gmail.com>
2019-10-21 20:39:19 +0100

    Done with splash screen animation

--- app/src/main/java/com/aad_team_42/travelmanticsrebranded/views/activities/SplashScreenActivity.java
+++ app/src/main/java/com/aad_team_42/travelmanticsrebranded/views/activities/SplashScreenActivity.java
@@ -2,19 +2,28 @@ package com.aad_team_42.travelmanticsrebranded.views.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.aad_team_42.travelmanticsrebranded.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    ImageView logo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        logo  = findViewById(R.id.app_logo);
+
+        setAnimation(logo);
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
@@ -27,6 +36,14 @@ public class SplashScreenActivity extends AppCompatActivity {
         }, 2000);
 
 
+    }
+
+    private void setAnimation(View view){
+
+        Animator animator = AnimatorInflater.loadAnimator(this, R.animator.set);
+
+        animator.setTarget(view);
+        animator.start();
     }
 
 }
